6ca0d7e36c1379edcb8443b41beb9361465644ac
Pennyk <denis.107@icloud.com>
2023-05-17 13:00:33 +0300

    изменил

--- ToDay/Form1.Designer.cs
+++ ToDay/Form1.Designer.cs
@@ -1,5 +1,7 @@
-﻿namespace ToDay {
-    partial class Form1 {
+﻿namespace ToDay
+{
+    partial class Form1
+    {
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -9,8 +11,10 @@
         ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -22,25 +26,28 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             button1 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(439, 75);
+            button1.Location = new Point(384, 56);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(189, 199);
+            button1.Size = new Size(165, 149);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "help";
             button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
--- ToDay/Form1.cs
+++ ToDay/Form1.cs
@@ -1,6 +1,9 @@
-namespace ToDay {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace ToDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
             InitializeComponent();
         }
     }
