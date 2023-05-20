3f606a91ccc65cb729bc54c00039e2f08d2396cf
Дарина <darusik21@gmail.com>
2023-05-21 00:04:04 +0300

    Решила конфликт

--- ToDay/Program.cs
+++ ToDay/Program.cs
@@ -10,11 +10,7 @@ namespace ToDay
         [STAThread]
         static void Main()
         {
-<<<<<<< HEAD
-            IRepository repository = new Repository();
-=======
             Repository repository = new Repository();
->>>>>>> 7290701 (Приняла)
             repository.ensureCreated();
 
              //To customize application configuration such as set high DPI settings or default font,
