90c883cc928abafcfbfea2dcc662099f174643a5
Дарина <darusik21@gmail.com>
2023-05-20 23:41:11 +0300

    Приняла

--- ToDay/Program.cs
+++ ToDay/Program.cs
@@ -10,10 +10,14 @@ namespace ToDay
         [STAThread]
         static void Main()
         {
+<<<<<<< HEAD
             IRepository repository = new Repository();
+=======
+            Repository repository = new Repository();
+>>>>>>> 7290701 (Приняла)
             repository.ensureCreated();
 
-            // To customize application configuration such as set high DPI settings or default font,
+             //To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
